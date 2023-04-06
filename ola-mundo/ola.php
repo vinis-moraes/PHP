93ffb088759c6ec0b510fd11f155bb22458a64b4
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-04-06 19:43:20 -0300

    Update

---
+++ ola-mundo/ola.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Exemplo PHP</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h2{
+            background-color: black;
+            color: white;
+        }
+        P{
+            color: blue;
+            background-color: gray;
+            font-weight: 600;
+            font-size: 2em;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php 
+    echo '<h2>Olá, Mundo!</h2>'; 
+    echo '<p>Teste</p>'
+    ?>
+</body>
+</html>
