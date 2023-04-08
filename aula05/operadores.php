ba21529bad18f59e370ccd83e46ccee0300653dc
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-04-08 10:43:11 -0300

    Add aula05/

---
+++ aula05/operadores.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $n1 = 3;
+    $n2 = 2;
+    echo "O primeiro número é $n1 e o segundo número é $n2 <br>";
+    echo "A soma vale ". ($n1+$n2). ".<br>" ;
+    echo "A subtração vale ". ($n1-$n2). ".<br>";
+    echo "A multiplicação vale ". ($n1*$n2). ".<br>";
+    echo "A divisão vale ". ($n1/$n2). ".<br>";
+    echo "O resto da divisão vale ". ($n1%$n2). ".<br>";
+    ?>
+</body>
+</html>
---
+++ aula05/somador.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somador</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Somador</h1>
+    <?php
+    $n1 = 3;
+    $n2 = 2;
+
+    $s = $n1 + $n2;;
+    echo "A soma entre $n1 e $n2 é $s"
+    ?>
+</body>
+</html>
